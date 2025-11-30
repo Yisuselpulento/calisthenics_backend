ebced83e8b9a9776833c8a8a614ba5cb7d6bdb2c
Monsster <hefestoalan@gmail.com>
2025-11-30 16:03:14 -0300

    add

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-
-
-		
-	} catch (error) {
-		console.log("Error connection to MongoDB: ", error.message);
-		process.exit(1); 
-	}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error.message);
+    // No matamos el proceso
+    // Opcional: intentar reconectar cada X segundos
+    setTimeout(connectDB, 5000); // reintenta cada 5s
+  }
 };
