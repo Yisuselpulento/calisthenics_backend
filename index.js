cdb6cfe8b46ca03fcade8f4806ac36984d95e304
Monsster <hefestoalan@gmail.com>
2026-01-15 12:39:58 -0300

    cors app

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // -------------------- Middleware --------------------
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); 
+/* app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));  */
 
-/*  app.use(cors()); */
+ app.use(cors()); 
 app.use(express.json());
 app.use(cookieParser());
 
